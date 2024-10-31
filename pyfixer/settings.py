c64e2e68559d93d2c555282da4ac2cc271285abc
Othman Boujendar <othman.boujendar@gmail.com>
2024-10-31 15:24:23 +0000

    Update settings.py

--- pyfixer/settings.py
+++ pyfixer/settings.py
@@ -2,7 +2,7 @@
 # Settings file for the pyfixer package
 
 API_HOME = 'http://data.fixer.io/api/'
-API_KEY = 'e921faef4c3dc034fd5f0673144b43b6'
+API_KEY = 'XXXXXXXX'
 BASE_CURRENCY = 'EUR'
 CURRENCIES = ['GBP', 'USD']
 DB_URI = 'sqlite:///db.sqlite3'
